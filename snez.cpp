dc0c509fcff6f379fff0d535d6cfd2e0fe5838b5
Steven <111824122+Cosmilax@users.noreply.github.com>
2022-09-15 19:22:36 -0700

    Update snez.cpp

--- snez.cpp
+++ snez.cpp
@@ -2,8 +2,11 @@
 //Date: September 13, 2022
 
 #include <iostream>
-namespace snez {
-void file_changes(){
+namespace snez 
+{
+    void file_changes()
+    {
     std::cout << "Hello, this is Steven's Source file\n";
+    }
 }
-}
+
