84c1492ddb0293b78a4bdd5bb43e538add7b1451
Rodolfo (Rudy) Velasquez <rvelasqu_2000@yahoo.com>
2022-10-04 16:13:51 -0700

    add personal files

--- rvelasquez.cpp
+++ rvelasquez.cpp
@@ -1,16 +1,28 @@
-//Rudy Velasquez 
-#include <iostream>
+//name: Rodolfo Velasquez 
+//file: rvelasquez.cpp
+//
+//Team 3 credits screen
+#include <X11/Xlib.h>
+#include <GL/glx.h>
 
-namespace rvelasquez
+unsigned int managed_state(unsigned int c)
 {
-	void world()
-	{
-		std::cout << "Hello World" << std::endl;
-
-	}
+	c = c ^ 1;
+	return c;
 }
 
-
-
+void show_credits(int xres, int yres)
+{	
+	int xcent = xres / 2;
+	int ycent = yres / 2;
+	int w = 200;
+	glColor3f(0.57, 0.82, 1.0);
+	glBegin(GL_QUADS);
+		glVertex2f(xcent-w, ycent-w);
+		glVertex2f(xcent-w, ycent+w);
+		glVertex2f(xcent+w, ycent+w);
+		glVertex2f(xcent+w, ycent-w);
+	glEnd();
+}
 
 
