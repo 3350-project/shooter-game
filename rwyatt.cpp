40c603ba0c459d727086a63058b80cbcb151d3af
g1ver <consigner0@gmail.com>
2022-10-12 09:51:44 -0700

    change namespace to class

--- rwyatt.cpp
+++ rwyatt.cpp
@@ -1,18 +1,59 @@
 // Reid Wyatt
 #include <iostream>
+#include <GL/glx.h>
 #include "rwyatt.h"
 
-void rwyatt::flip_state(bool &state)
+/*
+ * STATIC METHODS
+ */
+void RWyatt::flip_state(bool &state)
 {
     state = !state;
 }
 
-void rwyatt::pause_screen(bool &paused) 
+void RWyatt::pause_screen(bool &paused) 
 {
-    flip_state(paused);
+    RWyatt::flip_state(paused);
 }
 
-void rwyatt::network_mode(bool &networked) 
+void RWyatt::draw_border(int xres, int yres)
 {
-    flip_state(networked);
+    // draw a border using a triangle strip
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glColor4f(1.0, 1.0, 0.0, 0.9);
+    int w = 20;
+    glBegin(GL_TRIANGLE_STRIP);
+        glVertex2i(0, 0);
+        glVertex2i(0+w, w);
+        glVertex2i(0, yres);
+        glVertex2i(0+w, yres-w);
+        glVertex2i(xres, yres);
+        glVertex2i(xres-w, yres-w);
+        glVertex2i(xres, 0);
+        glVertex2i(xres-w, w);
+        glVertex2i(0, 0);
+        glVertex2i(0+w, w);
+    glEnd();
+    glDisable(GL_BLEND);
+}
+
+/*
+ * CLASS METHODS
+ */
+RWyatt::RWyatt() {
+    network_state = false;
+}
+
+bool RWyatt::startNetworking() 
+{
+    this->network_state = true;
+
+    // TODO: network client
+    return EXIT_FAILURE;
+}
+
+bool RWyatt::networked()
+{
+    return network_state;
 }
--- rwyatt.h
+++ rwyatt.h
@@ -1,15 +1,19 @@
 // Reid Wyatt
-
 #ifndef RWYATT_H
 #define RWYATT_H
 
-class rwyatt 
+class RWyatt
 {
 private:
     static void flip_state(bool &state);
+    static void draw_border(int xres, int yres);
+    bool network_state;
 public:
     static void pause_screen(bool &paused);
-    static void network_mode(bool &networked);
+    bool startNetworking();
+    bool stopNetworking();
+    bool networked();
+    RWyatt();
 };
 
 #endif
