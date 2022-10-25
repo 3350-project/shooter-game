fcc19de58611dcc9722eab4c834f8df8c3d08cdd
g1ver <consigner0@gmail.com>
2022-10-25 09:50:22 -0700

    add multiplayer feature drawing

--- rwyatt.cpp
+++ rwyatt.cpp
@@ -22,7 +22,7 @@ void RWyatt::draw_border(int xres, int yres)
     // draw a border using a triangle strip
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
-    glColor4f(1.0, 1.0, 0.0, 0.9);
+    glColor4f(1.0, 1.0, 0.0, 0.3);
     int w = 20;
     glBegin(GL_TRIANGLE_STRIP);
         glVertex2i(0, 0);
@@ -48,6 +48,7 @@ RWyatt::RWyatt() {
 
 bool RWyatt::start_networking() 
 {
+    // check multiplayer bool
     this->network_state = true;
     // TODO: network client
     return EXIT_FAILURE;
@@ -55,6 +56,7 @@ bool RWyatt::start_networking()
 
 bool RWyatt::stop_networking() 
 {
+    // check multiplayer bool
     this->network_state = false;
     // TODO: network client
     return EXIT_FAILURE;
@@ -63,4 +65,53 @@ bool RWyatt::stop_networking()
 bool RWyatt::networked()
 {
     return network_state;
+}
+
+bool RWyatt::mutliplayer()
+{
+    return multiplayer;
+}
+
+std::string RWyatt::player_name()
+{
+    return user_name;
+}
+
+std::string RWyatt::server_name()
+{
+    return server_ip;
+}
+
+void RWyatt::draw_networking(int xres, int yres)
+{
+	Rect r;
+	r.bot = yres - 80;
+	r.left = xres / 2;
+	r.center = 0;
+
+    ggprint16(&r, 20, 0x00ffffff, "Reid's Multiplayer Feature");
+
+    if (this->multiplayer) {
+        ggprint16(&r, 20, 0x00ffffff, "Multiplayer Enabled");
+        ggprint16(&r, 20, 0x0000ff00, "Server: %s", 
+                this->server_name().c_str());
+        ggprint16(&r, 20, 0x0000ff00, "Name: %s", 
+                this->player_name().c_str());
+        ggprint16(&r, 20, 0x0000ff00, "Status: %s", 
+                this->networked() ? "Connected" : "Disconnected");
+    } else {
+        ggprint16(&r, 20, 0x00ff0000, "Multiplayer disabled");
+    }
+}
+
+void RWyatt::set_network_config(int argc, char* argv[])
+{
+    if (argc < 2) {
+        this->multiplayer = false;
+        return;
+    }
+
+    this->multiplayer = true;
+    this->server_ip = argv[1];
+    this->user_name = argv[2];
 }
--- rwyatt.h
+++ rwyatt.h
@@ -2,11 +2,16 @@
 #ifndef RWYATT_H
 #define RWYATT_H
 
+#include <string>
+
 class RWyatt
 {
 private:
     static void flip_state(bool &state);
     bool network_state;
+    bool multiplayer;
+    std::string user_name;
+    std::string server_ip;
 public:
     static void pause_screen(bool &paused);
     static void draw_border(int xres, int yres);
@@ -14,6 +19,10 @@ public:
     bool start_networking();
     bool stop_networking();
     bool networked();
+    bool mutliplayer();
+    void set_network_config(int, char*[]);
+    std::string player_name();
+    std::string server_name();
     RWyatt();
 };
 
